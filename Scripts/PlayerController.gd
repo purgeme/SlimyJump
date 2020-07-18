016e0f413c1f11d867dd12f82006b8c255adf017
Parvesh Kumar <parveshkumarbaloda@gmail.com>
2020-07-18 15:23:46 +0530

    Some corrections

--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -9,15 +9,22 @@ var velocityy = 0
 var angle = 0
 var on_ground = true
 
+# The particle system for the trailing effect
 onready var emitter = get_node("CollisionShape2D/Sprite/CPUParticles2D")
 
 var jumping = false
 
 signal player_is_dead
 
-func get_input(): # Implement Input
+
+# Get input for the player to move
+func get_input():
+	
+	# Reset the values before getting input
+	# To move the player only once if input comes only once
 	velocityx = 0
 	jumping = false
+
 	if Input.is_action_pressed("right"):
 		velocityx = 1
 	if Input.is_action_pressed("left"):
@@ -25,10 +32,17 @@ func get_input(): # Implement Input
 	if Input.is_action_pressed("jump"):
 		jumping = true
 
-func playdead():
-	get_tree().change_scene("res://MainScreen.tscn")
 
+# What to do when player is dead
+func playdead():
+	get_tree().change_scene("res://MainScreen.tscn") # Change scene to go back to main screen
+
+
+# Main process
 func _process(delta):
+
+	# Check if player is to die
+	# There are multiple conditions for player to die as there are many raycasts
 	if $RayCast2D.is_colliding() and $RayCast2D2.is_colliding():
 		emit_signal("player_is_dead")
 		playdead()
@@ -40,11 +54,16 @@ func _process(delta):
 		playdead()
 
 
+# The movement is controlled here to keep it consistant
 func _physics_process(delta):
-	# velocityx = 0
+	
 	get_input()
+
 	# Implement Gravity
 	if !self.is_on_floor():
+
+		# There is different gravity for going up and down
+		# to make the jump look a little more snappy
 		if velocityy < 0 :
 			velocityy += GRAVITY
 		else :
@@ -52,9 +71,13 @@ func _physics_process(delta):
 	else :
 		velocityy = 0
 		on_ground = true
+	
 	# Implement jumping
 	if jumping :
+
+		#The first 2 conditions are there for wall jumping
 		if self.is_on_floor() and ($Bouncers/LeftBouncer.is_colliding() and $Bouncers/RightBouncer.is_colliding()):
+			on_ground = false
 			velocityy = 0
 			velocityy -= JUMPING_SPEED
 			$Jump.play()
@@ -67,16 +90,13 @@ func _physics_process(delta):
 			velocityy = 0
 			velocityy -= JUMPING_SPEED
 			$Jump.play()
-	# Execute movement
+	
+	# Execute all of the movement
 	move_and_slide(Vector2(velocityx * MAX_SPEED, velocityy), Vector2(0, -1), false, 4, PI/4, false)
 	
 	emitter.set_gravity(Vector2(-velocityx * 10000, -velocityy  * 30))
-	
-	#if velocityx != 0 or velocityy != 0:
-	#	yield(get_tree().create_timer(0.5), "timeout")
-	#	emitter.emitting = true
-	#	emitter.visible = true
-	#	
-	#else :
-	#	emitter.emitting = false
-	#	emitter.visible = false
+
+
+
+func _on_FixWallJumping_timeout() -> void:
+	pass # Replace with function body.
