dea4bad7794faba3187b9b34d26178ed6ec0cee0
Parvesh Kumar <parveshkumarbaloda@gmail.com>
2020-07-20 18:11:23 +0530

    Improved and changed some assets

--- Scripts/QuitButton.gd
+++ Scripts/QuitButton.gd
@@ -3,10 +3,4 @@ extends Control
 var quit = false
 
 func _on_Button_pressed():
-	get_parent().get_parent().get_parent().get_node("Select").play()
-	quit = true
-
-
-func _on_Select_finished() -> void:
-	if quit:
-		get_tree().quit()
+	get_tree().quit()
